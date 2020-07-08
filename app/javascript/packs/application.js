beacfcf2412bdb1b6b4a45d3654b4acc1d860a57
Edwin Vasquez <edwin.vasquez@grupopdc.com>
2020-07-08 11:31:06 -0600

    Se agrega twitter bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "bootstrap"
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toogle="tooltip"]').tooltip()
+})
